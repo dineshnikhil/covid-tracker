e1941ebf74c69d517268cab628f1fb5edc5bbf38
DineshNikhil <dineshnikhil@pop-os.localdomain>
2022-01-13 09:59:48 +0530

    changed the home page login in app.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@ import  { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Nav from './compounents/Nav';
 import Home from './compounents/Home';
-import Article from './compounents/Article';
+// import Article from './compounents/Article';
 
 function App() {
   return (
     <div className="App bg-emerald-900">
       <Nav />
-      <Routes>
-        <Route path='/covid-tracker' element={<Home />} />
-        <Route path="/article/:id" element={<Article />} />
-      </Routes>
+      <main>
+        <Home />
+      </main>
     </div>
   );
 }
